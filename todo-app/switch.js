f9253faf9efa2b19ef7fd9c5fb7135d972809e9f
ayushkushwaha4527 <ayushkushwaha97291@gmail.com>
2025-01-17 17:12:11 +0530

    commit

---
+++ todo-app/switch.js
@@ -0,0 +1,13 @@
+function myRole(rollId){
+    switch(rollId){
+        case 1:
+            return "footballer"
+        case 2:
+            return "cricketer"
+        case 3:
+            return "acter"
+    }
+}
+console.log(myRole(1))
+console.log(myRole(2))
+console.log(myRole(3))
